d12277abcc9c581a8154be369ccaa9f547e95be5
postwo <codo7717@naver.com>
2025-03-05 19:30:49 +0900

    user dto 추가

---
+++ src/main/java/com/example/jwtoauthtoken/model/user/User.java
@@ -0,0 +1,15 @@
+package com.example.jwtoauthtoken.model.user;
+
+import com.example.jwtoauthtoken.model.entity.UserEntity;
+
+public record User(Long userId, String username, String name, String email) {
+
+    public static User from(UserEntity userEntity) {
+        return new User(
+                userEntity.getUserId(),
+                userEntity.getUsername(),
+                userEntity.getName(),
+                userEntity.getEmail());
+    }
+
+}
