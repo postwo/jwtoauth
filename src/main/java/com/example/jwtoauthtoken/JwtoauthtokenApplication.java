9f601158630e62f3a0cd8c749e465ad514f29f7d
postwo <codo7717@naver.com>
2025-03-05 19:11:11 +0900

    세팅

---
+++ src/main/java/com/example/jwtoauthtoken/JwtoauthtokenApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwtoauthtoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtoauthtokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtoauthtokenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jwtoauthtoken/JwtoauthtokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwtoauthtoken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtoauthtokenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
